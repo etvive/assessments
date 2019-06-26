91f86f0a496e4e60fd34c36e4c3762832180a799
etvive <etvive@gmail.com>
2019-06-26 10:04:56 +0200

    Add the repository class

---
+++ src/main/java/com/assessments/repositories/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.assessments.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.assessments.domain.Question;
+
+public interface QuestionRepository extends MongoRepository<Question, Long> {
+
+}
